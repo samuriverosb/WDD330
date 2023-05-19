5424cf8de235fe565a2d3309718f535218fc38d6
dallingilbert <djgilb24@byui.edu>
2023-05-19 15:43:49 -0600

    adding renderListWithTemplate function

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -30,3 +30,18 @@ export function getParam(param) {
   const product = urlParams.get(param);
   return product;
 }
+
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = true
+) {
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+
+  const htmlString = list.map(templateFn);
+  parentElement.insertAdjacentHTML(position, htmlString.join(""));
+}
