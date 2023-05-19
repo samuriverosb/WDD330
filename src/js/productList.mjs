358184ec7317cfb06e465f73ff84f7b692a0d3f5
dallingilbert <djgilb24@byui.edu>
2023-05-19 15:43:32 -0600

    dynamically rendering template

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -1,30 +1,25 @@
 import { getData } from "./productData.mjs";
-
-let tentsData = getData(category);
+import { renderListWithTemplate } from "./utils.mjs";
 
 export default function productList(category, selector) {
-    // get the element we will insert the list into from the selector
-    // get the list of products 
-    // render out the product list to the element
+  // get the element we will insert the list into from the selector
+  let element = document.querySelector(selector);
+  // get the list of products
+  let products = getData(category);
+  // render out the product list to the element
+  renderListWithTemplate(productCardTemplate, element, products);
 }
-
 
 function productCardTemplate(product) {
-    return `<li class="product-card">
-    <a href="product_pages/index.html?product=">
+  return `<li class="product-card">
+    <a href="product_pages/index.html?product=${product.Id}">
       <img
-        src=""
-        alt="Image of"
+        src="${product.Image}"
+        alt="Image of ${product.Name}"
       />
-      <h3 class="card__brand"></h3>
-      <h2 class="card__name"></h2>
-      <p class="product-card__price">$</p></a
+      <h3 class="card__brand">${product.Brand.Name}</h3>
+      <h2 class="card__name">${product.NameWithoutBrand}</h2>
+      <p class="product-card__price">${product.FinalPrice}</p></a
     >
-  </li>`
-}
-
-function renderList() {
-    array.forEach(element => {
-        
-    });
+  </li>`;
 }
