883e255b746141567eb68b3630d13521b452f610
dallingilbert <djgilb24@byui.edu>
2023-05-19 19:52:02 -0600

    dynamically rendering cart items

---
+++ src/js/shoppingCart.mjs
@@ -0,0 +1,30 @@
+import { getLocalStorage, renderListWithTemplate } from "./utils.mjs";
+
+export default function shoppingCart() {
+  // creating var to store local storage
+  const cartItems = [];
+  cartItems.push(getLocalStorage("so-cart"));
+  console.log(cartItems);
+  let outputEl = document.querySelector(".product-list");
+  // render out the product list to the element
+  renderListWithTemplate(cartItemTemplate, outputEl, cartItems);
+}
+
+function cartItemTemplate(item) {
+  const newItem = `<li class="cart-card divider">
+  <a href="#" class="cart-card__image">
+    <img
+      src="${item.Image}"
+      alt="${item.Name}"
+    />
+  </a>
+  <a href="#">
+    <h2 class="card__name">${item.Name}</h2>
+  </a>
+  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__price">${item.FinalPrice}</p>
+</li>`;
+
+  return newItem;
+}
