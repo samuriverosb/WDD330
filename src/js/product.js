67dc40a427c89d69a6392a1eb158d60753dbb335
dallingilbert <djgilb24@byui.edu>
2023-05-19 19:47:40 -0600

    dynamically rendering the header and footer

--- src/js/product.js
+++ src/js/product.js
@@ -1,5 +1,8 @@
 import { getParam } from "./utils.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 import productDetails from "./productDetails.mjs";
+
+loadHeaderFooter();
 
 const productId = getParam("product");
 productDetails(productId);
