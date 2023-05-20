a3237b22cfbad5d01f400a1f28d0459b09b6f172
dallingilbert <djgilb24@byui.edu>
2023-05-19 19:49:43 -0600

    dynamically rendering cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,52 +1,5 @@
-import { getLocalStorage } from "./utils.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
+import shoppingCart from "./shoppingCart.mjs";
 
-function renderCartContents() {
-  // Change cartItems to an array
-  const cartItems = [];
-
-  const localStore = getLocalStorage("so-cart");
-
-  // Push local storage onto array
-  if (localStore !== null) {
-    cartItems.push(localStore);
-  }
-
-  // Check cart
-  if (checkCartContents(cartItems)) {
-    // Do not map items if cart is empty
-    const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-    document.querySelector(".product-list").innerHTML = htmlItems.join("");
-  } else {
-    return;
-  }
-}
-
-function checkCartContents(cart) {
-  // Return false if cart contents are empty
-  if (cart === null) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
-function cartItemTemplate(item) {
-  const newItem = `<li class="cart-card divider">
-  <a href="#" class="cart-card__image">
-    <img
-      src="${item.Image}"
-      alt="${item.Name}"
-    />
-  </a>
-  <a href="#">
-    <h2 class="card__name">${item.Name}</h2>
-  </a>
-  <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
-  <p class="cart-card__price">$${item.FinalPrice}</p>
-</li>`;
-
-  return newItem;
-}
-
-renderCartContents();
+loadHeaderFooter();
+shoppingCart();
