1c30c28e45c808983dacc9fd8ccecfa55a82c54b
dallingilbert <djgilb24@byui.edu>
2023-05-19 19:49:26 -0600

    template functionality for header and footer

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -44,3 +44,50 @@ export function renderListWithTemplate(
   const htmlString = list.map(templateFn);
   parentElement.insertAdjacentHTML(position, htmlString.join(""));
 }
+
+export async function renderWithTemplate(
+  templateFn,
+  parentElement,
+  data,
+  callback,
+  position = "afterbegin",
+  clear = true
+) {
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+
+  const htmlString = await templateFn(data);
+  parentElement.insertAdjacentHTML(position, htmlString);
+
+  if (callback) {
+    callback(data);
+  }
+
+  // const htmlString = data.map(templateFn);
+  // parentElement.insertAdjacentHTML(position, htmlString.join(""));
+}
+
+export function loadTemplate(path) {
+  return async function () {
+    const res = await fetch(path);
+    if (res.ok) {
+      const html = await res.text();
+      return html;
+    }
+  };
+}
+
+export async function loadHeaderFooter() {
+  // load the header and footer templates from partials
+  const footerTemplateFn = loadTemplate("/partials/footer.html");
+  const headerTemplateFn = loadTemplate("/partials/header.html");
+
+  // Grab header and footer elements out of the DOM
+  let footer_el = document.querySelector("footer");
+  let header_el = document.querySelector("#main-header");
+
+  // Render header and footer (renderWithTemplate)
+  renderWithTemplate(footerTemplateFn, footer_el);
+  renderWithTemplate(headerTemplateFn, header_el);
+}
