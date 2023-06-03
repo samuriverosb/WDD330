230af0cb327526d1e9585079cd19b1105ce54afc
dallingilbert <djgilb24@byui.edu>
2023-06-02 22:08:28 -0600

    fixed the sites broken links and images

--- src/js/productData.mjs
+++ src/js/productData.mjs
@@ -1,4 +1,4 @@
-const baseURL = import.meta.env.VITE_SERVER_URL
+const baseURL = import.meta.env.VITE_SERVER_URL;
 
 function convertToJson(res) {
   if (res.ok) {
@@ -8,26 +8,18 @@ function convertToJson(res) {
   }
 }
 
-// export async function getData(category = "tents") {
-//   return fetch(`../json/${category}.json`)
-//     .then(convertToJson)
-//     .then((data) => data);
-// }
 export async function getData(category) {
   const response = await fetch(baseURL + `products/search/${category}`);
   const data = await convertToJson(response);
 
-  console.log("data from getData", data);
+  //console.log("data from getData", data);
 
   return data.Result;
 }
 
 export async function findProductById(id) {
-
   const response = await fetch(baseURL + `product/${id}`);
   const data = await convertToJson(response);
 
-  // const products = await getData();
-  // return products.find((item) => item.Id === id);
-  return data.Result
+  return data.Result;
 }
