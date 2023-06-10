9d2d2e051c5477eaa68e320595c81926f8dd14f3
pumecart <car21132@byui.edu>
2023-06-09 21:04:14 -0400

    finalize wk7 and start wk8

--- src/js/checkoutProcess.mjs
+++ src/js/checkoutProcess.mjs
@@ -1,9 +1,32 @@
+import { checkout } from "./externalServices.mjs";
 import { getLocalStorage } from "./utils.mjs";
 
 // takes the items currently stored in the cart (localstorage) and returns them in a simplified form.
 function packageItems(items) {
   // convert the list of products from localStorage to the simpler form required for
   // the checkout process. Array.map would be perfect for this.
+  const simplifiedItems = items.map((item) => {
+    console.log(item);
+    return {
+      id: item.Id,
+      price: item.FinalPrice,
+      name: item.Name,
+      quantity: 1
+    }
+  })
+
+  return simplifiedItems;
+}
+
+function formDataToJSON(formElement) {
+  const formData = new FormData(formElement),
+    convertedJSON = {};
+
+  formData.forEach(function (value, key) {
+    convertedJSON[key] = value;
+  });
+
+  return convertedJSON;
 }
 
 const checkoutProcess = {
@@ -37,19 +60,42 @@ const checkoutProcess = {
     }
 
     // calculate orderTotal
-    this.orderTotal = this.tax + this.shipping + this.itemTotal;
-    return this.orderTotal;
+    this.orderTotal = (
+      parseFloat(this.tax) + 
+      parseFloat(this.shipping) + 
+      parseFloat(this.itemTotal)).toFixed(2);
   },
   // function to display total
   displayOrderTotals: function () {
     // once the totals are all calculated display them in the order
     // summary page
-    const orderForm = document.getElementById("orderForm");
+    const shipping = document.querySelector(this.outputSelector + " #shipping");
+    const tax = document.querySelector(this.outputSelector + " #tax");
+    const orderTotal = document.querySelector(
+      this.outputSelector + " #orderTotal"
+    );
+    shipping.innerText = "$" + this.shipping;
+    tax.innerText = "$" + this.tax;
+    orderTotal.innerText = "$" + this.orderTotal;
   },
+    
   checkout: async function (form) {
     // build the data object from the calculated fields, the items in
     // the cart, and the information entered into the form call the
     // checkout method in our externalServices module and send it our data object.
+    const checkoutJSON = formDataToJSON(form)
+    checkoutJSON.orderDate = new Date();
+    checkoutJSON.orderTotal = this.orderTotal;
+    checkoutJSON.tax = this.tax;
+    checkoutJSON.shipping = this.shipping;
+    checkoutJSON.items = packageItems(this.list);
+    console.log(json);
+    try {
+      const res = await checkout(json);
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
   },
 };
 
